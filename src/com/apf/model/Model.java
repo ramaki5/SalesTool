c5ada94413030073fbb5ce04f6a94f966dad79bc
ramaki5 <rk.mundru@gmail.com>
2017-12-27 16:33:55 +0530

    First Commit

---
+++ src/com/apf/model/Model.java
@@ -0,0 +1,10 @@
+package com.apf.model;
+
+public class Model {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/apf/view/View.java
@@ -0,0 +1,10 @@
+package com.apf.view;
+
+public class View {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
